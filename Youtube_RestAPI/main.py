901c914577aac0b88f3aad9f3aeb96e52b462656
theresale09 <phnguyen0912@gmail.com>
2021-01-21 10:12:59 -0600

    patch is working

--- Youtube_RestAPI/main.py
+++ Youtube_RestAPI/main.py
@@ -1,48 +1,80 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-db.create_all() #create database
-
 class VideoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     views = db.Column(db.Integer, nullable=False)
     likes = db.Column(db.Integer, nullable=False)
 
+    def __repr__(self):
+        return f"Video(name={name}, views = {views}, likes = {likes})"
+
+
+#RequestParse() automatically fills in each argument with the value "None" even if they aren't passed through
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required!", required=True)
-video_put_args.add_argument("views", type=int, help="Views of the video")
-video_put_args.add_argument("likes", type=int, help="Likes on the video")
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser() #parse arguments for update, each argument is optional 
+video_update_args.add_argument("name", type=str, help="Name of the video is required!")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Could not find video...")
 
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video already exists with that ID... ")
+#serialize Model
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields) #takes result and serialize it using resource fields. (convert from VideoModel -> json serializable)
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id = video_id).first() #filter by video id and return the first entry of filter_by
+        if not result:
+            abort(404, message="Could not find video with that id...")  
+        return result
 
+    @marshal_with(resource_fields) #serialize response so we can return it
     def put(self, video_id):
-        abort_if_video_exists(video_id)
-        args = video_put_args.parse_args()        
-        videos[video_id] = args
-        return videos[video_id], 201 
+        args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first() #check if video id already exists
+        if result:
+            abort(409, message="Video id taken...") #abort if it already exists
+        video = VideoModel(id = video_id, name = args['name'], views = args['views'], likes = args['likes']) #create video
+        db.session.add(video) #add current object to database session, temporarily posting this video
+        db.session.commit() #commit any changes to this session, permanently posting this video
+        return video, 201 
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id): #update
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first() #check if video already exists
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update...")
+        
+        if args['name']: #check if this key doesn't have a None value
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit() #once the object is already in the database, no need to readd it and can just commit any changes we made to the database
+
+        return result
+
 
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        del videos[video_id]
         return '', 204
         
 
--- Youtube_RestAPI/test.py
+++ Youtube_RestAPI/test.py
@@ -6,22 +6,14 @@ data = [{"name": "My First Video", "views": 10000, "likes": 10},
         {"name": "My second video", "views": 20000, "likes": 50},
         {"name": "How to make REST API in Python", "views": 30000, "likes": 100}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+response = requests.patch(BASE + "video/2", {"name": "Tim", "views": 100, "likes": 35})
+print(response.json())
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
 
 input()
-
-for i in range(len(data)):
-    response = requests.get(BASE + "video/" + str(i))
-    print(response.json())
-
-input()
-
-response = requests.delete(BASE + "video/0")
-print(response)
-
-for i in range(len(data)):
-    response = requests.get(BASE + "video/" + str(i))
-    print(response.json())
+response = requests.get(BASE + "video/2")
+print(response.json())
 
